5d77a44739678ef0d9f514387206caf30a437118
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-09-04 12:01:25 +0200

    Adjusted rebuild.sh to reflect changes

--- rebuild.sh
+++ rebuild.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-docker stop meteor-website
-docker stop mnd-website
-docker stop prerender
-docker rm meteor-website
-docker rm mnd-website
-docker rm prerender
-nodejs cmc.js meteor-website meteor-website.options.json
-nodejs cmc.js mnd-website mnd-website.options.json
-nodejs cmc.js prerender prerender.options.json
+nodejs cmc.js meteor-website
+nodejs cmc.js mnd-website
+nodejs cmc.js prerender
