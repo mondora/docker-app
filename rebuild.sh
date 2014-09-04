e5808bf270a45af0c33cf17f077a33a3490673aa
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-09-04 11:36:01 +0200

    Fixed wrong command to docker

--- rebuild.sh
+++ rebuild.sh
@@ -2,9 +2,9 @@
 docker stop meteor-website
 docker stop mnd-website
 docker stop prerender
-docker remove meteor-website
-docker remove mnd-website
-docker remove prerender
+docker rm meteor-website
+docker rm mnd-website
+docker rm prerender
 nodejs cmc.js meteor-website meteor-website.options.json
 nodejs cmc.js mnd-website mnd-website.options.json
 nodejs cmc.js prerender prerender.options.json
